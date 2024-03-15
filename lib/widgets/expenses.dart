242f3827cf7f33902c7b3eaf3e2dbfd8027a6bd7
Tejada <tejadacodes@gmail.com>
2024-03-14 21:13:49 -0500

    Expense form and save

---
+++ lib/widgets/expenses.dart
@@ -0,0 +1,66 @@
+import 'package:expenses/widgets/expenses_list/expenses_list.dart';
+import 'package:expenses/models/expense.dart';
+import 'package:expenses/widgets/new_expense.dart';
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'Flutter course',
+      amount: 19.5,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: 'Vegetables',
+      amount: 5.59,
+      date: DateTime.now(),
+      category: Category.food,
+    ),
+  ];
+
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+      context: context,
+      builder: (ctx) => NewExpense(onAddExpense: _addExpense),
+    );
+  }
+
+  void _addExpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.add(expense);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Expenses Tracker'),
+        actions: [
+          IconButton(
+            onPressed: _openAddExpenseOverlay,
+            icon: const Icon(Icons.add),
+          )
+        ],
+      ),
+      body: Column(
+        children: [
+          const Text('The chart'),
+          Expanded(
+            child: ExpensesList(expenses: _registeredExpenses),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -0,0 +1,17 @@
+import 'package:expenses/models/expense.dart';
+import 'package:expenses/widgets/expenses_list/expense_item.dart';
+import 'package:flutter/material.dart';
+
+class ExpensesList extends StatelessWidget {
+  const ExpensesList({super.key, required this.expenses});
+
+  final List<Expense> expenses;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: expenses.length,
+      itemBuilder: (ctx, index) => ExpenseItem(expenses[index]),
+    );
+  }
+}
